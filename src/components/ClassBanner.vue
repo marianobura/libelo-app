7327a5ce37937b1b7d7337834584e268224ef8d2
marianobura <>
2024-11-13 09:25:56 -0300

    Update ClassBanner text and integrate ClassBanner into CalendarView

--- src/components/ClassBanner.vue
+++ src/components/ClassBanner.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="flex flex-col justify-end p-3 h-28 w-full bg-red-800 rounded-xl">
-        <div class="text-white font-semibold text-xl uppercase">Literatura</div>
+        <div class="text-white font-semibold text-xl uppercase">Materia</div>
     </div>
 </template>
