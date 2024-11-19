499364dddbc629c65d3bed95ae44bb21fb85b0ce
marianobura <marianoobura@gmail.com>
2024-11-19 20:46:48 -0300

    Refactor settings view and base item component

--- src/components/Settings/BaseItem.vue
+++ src/components/Settings/BaseItem.vue
@@ -5,28 +5,15 @@ import { defineProps } from 'vue';
 defineProps({
     title: String,
     icon: Function,
-    account: {
-        type: Boolean,
-        default: false
-    },
 });
 </script>
 
 <template>
     <div class="flex items-center justify-between bg-white w-full p-3 rounded-xl">
         <div class="flex gap-2 items-center">
-            <template v-if="account">
-                <img src="https://avatar.iran.liara.run/public/2" alt="Imagen de perfil" class="size-12">
-                <div class="flex flex-col">
-                    <p class="font-semibold">Mariano Buranits</p>
-                    <p class="text-sm text-neutral-500">mariano.buranits@davinci.edu.ar</p>
-                </div>
-            </template>
-            <template v-else>
-                <div class="flex items-center justify-center bg-neutral-200/60 size-10 rounded-lg">
-                    <component :is="icon" :size="24" />
-                </div>
-            </template>
+            <div class="flex items-center justify-center bg-neutral-200/60 size-10 rounded-lg">
+                <component :is="icon" :size="24" />
+            </div>
             <p>{{ title }}</p>
         </div>
         <ChevronRight :size="24" />
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -3,29 +3,15 @@ import BaseBody from "@/components/BaseBody.vue";
 import BaseNav from "@/components/BaseNav.vue";
 import LabelTitle from "@/components/Settings/LabelTitle.vue";
 import BaseItem from "@/components/Settings/BaseItem.vue";
-import { Book, BookOpenCheck, CalendarDays, Goal, Languages, LayoutDashboard, MessageSquareDot, NotebookPen } from "lucide-vue-next";
+import { UserRoundPen, SquareAsterisk, LayoutDashboard } from "lucide-vue-next";
 
 const SHORTCUTS = {
     profile: {
         label: "Perfil",
-        special: true,
-    },
-    learning: {
-        label: "Aprendizaje",
         items: [
-            { title: "Materias", icon: Book },
-            { title: "Objetivos", icon: Goal },
-            { title: "Calendario", icon: CalendarDays },
-            { title: "Evaluaciones", icon: BookOpenCheck },
-            { title: "Ejercicios", icon: NotebookPen },
-        ],
-    },
-    general: {
-        label: "General",
-        items: [
-            { title: "Notificaciones", icon: MessageSquareDot },
-            { title: "Idioma", icon: Languages },
-            { title: "Personalización", icon: LayoutDashboard },
+            { title: "Editar perfil", icon: UserRoundPen },
+            { title: "Cambiar contraseña", icon: SquareAsterisk },
+            { title: "Google Classroom", icon: LayoutDashboard },
         ],
     },
 }
@@ -37,13 +23,8 @@ const SHORTCUTS = {
         <div class="flex flex-col gap-4 p-2">
             <template v-for="(category, key) in SHORTCUTS" :key="key">
                 <LabelTitle :label="category.label">
-                    <template v-if="category.special">
-                        <BaseItem account />
-                    </template>
-                    <template v-else>
-                        <template v-for="(item, index) in category.items" :key="index">
-                            <BaseItem :title="item.title" :icon="item.icon" />
-                        </template>
+                    <template v-for="(item, index) in category.items" :key="index">
+                        <BaseItem :title="item.title" :icon="item.icon" />
                     </template>
                 </LabelTitle>
             </template>
