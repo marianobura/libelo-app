9d0fc11d226fc6958353d85ad0cacecb075cc886
marianobura <marianoobura@gmail.com>
2024-11-24 22:01:51 -0300

    Refactor background images and typography styles

--- src/components/Home/HomeModal.vue
+++ src/components/Home/HomeModal.vue
@@ -40,10 +40,7 @@ const handleAddSubject = () => {
             </div>
             <hr>
             <div class="py-2">
-                <div class="relative">
-                    <input v-model="subjectName" type="subject" id="hs-floating-input-subject" class="peer p-4 block w-full border border-neutral-500 rounded-lg text-sm placeholder:text-transparent focus:border-libelo-500 focus:ring-libelo-500 disabled:opacity-50 disabled:pointer-events-none focus:pt-6 focus:pb-2 [&:not(:placeholder-shown)]:pt-6 [&:not(:placeholder-shown)]:pb-2 autofill:pt-6 autofill:pb-2" placeholder="Agregar nueva materia">
-                    <label for="hs-floating-input-subject" class="absolute top-0 start-0 p-4 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent origin-[0_0] peer-disabled:opacity-50 peer-disabled:pointer-events-none peer-focus:scale-90 peer-focus:translate-x-0.5 peer-focus:-translate-y-1.5 peer-focus:text-gray-500 peer-[:not(:placeholder-shown)]:scale-90 peer-[:not(:placeholder-shown)]:translate-x-0.5 peer-[:not(:placeholder-shown)]:-translate-y-1.5 peer-[:not(:placeholder-shown)]:text-gray-500">Agregar nueva materia</label>
-                </div>
+                <input v-model="subjectName" type="text" class="w-full border border-neutral-400 rounded-xl p-3 " placeholder="Ingrese una nueva materia">
             </div>
             <div class="py-2">
                 <BaseButton @click="handleAddSubject" primary>Agregar materia</BaseButton>
