ac072f98da600a70038950fe2a89b19f6fbf83f2
marianobura <marianoobura@gmail.com>
2025-01-15 21:58:39 -0300

    Remove console.log statement in LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -61,8 +61,6 @@ const handleLogin = async () => {
         const emailUrl = new URL(`/api/users/email/${email.value}`, process.env.VUE_APP_API_URL);
         const emailResponse = await axios.get(emailUrl.toString());
 
-        console.log(emailResponse.data.data.google);
-
         if (emailResponse.data.data.google) {
             errorMessage.value = 'Este correo está asociado a una cuenta de Google. Por favor, inicie sesión con Google.';
             loading.value = false;
