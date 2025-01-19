fb33d80dc5b76475512b6a33a0d8ac7a9b33f07e
marianobura <marianoobura@gmail.com>
2025-01-19 00:34:04 -0300

    Add margin-bottom to welcome view

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -9,7 +9,7 @@ import BaseButton from '@/components/BaseButton.vue';
             <WelcomeSvg />
         </div>
         <div class="flex flex-col justify-between p-2">
-            <div class="flex flex-col items-center gap-2">
+            <div class="flex flex-col items-center gap-2 mb-4">
                 <h1 class="font-semibold text-2xl text-neutral-900">¡Bienvenido a Libelo!</h1>
                 <p class="text-neutral-700 text-balance text-center">Estamos encantados de ayudarte a mejorar tu aprendizaje de manera sencilla y eficaz.</p>
             </div>
