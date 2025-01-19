ebe1be3103952bc0fb0f0fc138651d7eee5b32c4
marianobura <marianoobura@gmail.com>
2025-01-18 22:54:27 -0300

    Refactor loading state in SubjectBanner, FastChatView and HomeView components

--- src/components/SubjectBanner.vue
+++ src/components/SubjectBanner.vue
@@ -1,16 +1,20 @@
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { useSubjectData } from "@/services/subjectData"; 
 
 const { subjectData, fetchSubjectData } = useSubjectData();
 
-onMounted(() => {
-    fetchSubjectData();
+const loading = ref(true);
+
+onMounted(async () => {
+    await fetchSubjectData();
+    loading.value = false;
 });
 </script>
 
 <template>
     <div class="flex items-end p-3 h-28 w-full bg-red-800 rounded-xl">
-        <div class="text-white font-semibold text-xl uppercase break-all line-clamp-1">{{ subjectData?.name }}</div>
+        <div v-if="loading" class="text-white font-semibold text-xl uppercase break-all line-clamp-1">Cargando...</div>
+        <div v-else class="text-white font-semibold text-xl uppercase break-all line-clamp-1">{{ subjectData?.name }}</div>
     </div>
 </template>
