694b8e058159fdf412f1c1f63ed3e5e978404e79
marianobura <marianoobura@gmail.com>
2025-01-18 23:59:55 -0300

    Refactor App.vue: Remove unused code and fix formatting

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,11 @@
 <script setup>
-import { onMounted, onBeforeUnmount } from 'vue';
+import { onMounted } from 'vue';
 import { useUserStore } from './stores/userStore';
 
 const userStore = useUserStore();
 
 onMounted(() => {
     userStore.fetchUser();
-
-    const preventReload = (e) => {
-        if (e.touches[0].clientY < 50) {
-            e.preventDefault();
-        }
-    };
-
-    window.addEventListener('touchmove', preventReload, { passive: false });
-
-    onBeforeUnmount(() => {
-        window.removeEventListener('touchmove', preventReload);
-    });
 });
 </script>
 
