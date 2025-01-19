f2a465ae8435fd3e489a72a59665b39155df9d4c
marianobura <marianoobura@gmail.com>
2025-01-18 21:39:04 -0300

    Add LibeloLogo component and update HomeNav logo

--- src/components/Home/HomeNav.vue
+++ src/components/Home/HomeNav.vue
@@ -1,12 +1,13 @@
 <script setup>
 import UserAvatar from '@/components/UserAvatar.vue';
+import LibeloLogo from '@/assets/LibeloLogo.vue'
 // import { Bell } from 'lucide-vue-next';
 </script>
 
 <template>
     <div class="flex justify-between items-center px-2 bg-white border-b border-gray-300">
-        <div>
-            <img src="/img/logo.png" alt="Logo de Libelo" class="h-8">
+        <div class="w-10 text-libelo-500">
+            <LibeloLogo />
         </div>
         <div class="flex gap-2 items-center">
             <!-- <div class="flex items-center justify-center size-10 bg-gray-100 rounded-full">
