b277f63cd5e7f170888b33caef4fb3e1da944612
marianobura <marianoobura@gmail.com>
2025-01-18 23:36:39 -0300

    Add touchmove event listener to prevent page reload on swipe

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, onBeforeUnmount } from 'vue';
 import { useUserStore } from './stores/userStore';
 
 const userStore = useUserStore();
 
 onMounted(() => {
     userStore.fetchUser();
+
+    const preventReload = (e) => {
+        if (e.touches[0].clientY < 50) {
+            e.preventDefault();
+        }
+    };
+
+    window.addEventListener('touchmove', preventReload, { passive: false });
+
+    onBeforeUnmount(() => {
+        window.removeEventListener('touchmove', preventReload);
+    });
 });
 </script>
 
