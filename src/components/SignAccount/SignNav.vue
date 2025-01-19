034a40d64d984344fa00629abc1ff61cc9245eb7
marianobura <marianoobura@gmail.com>
2025-01-19 00:33:58 -0300

    Update logo imports in HomeNav.vue and SignNav.vue

--- src/components/SignAccount/SignNav.vue
+++ src/components/SignAccount/SignNav.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { defineProps } from 'vue';
+import LibeloLogotipo from '@/assets/LibeloLogotipo.vue'
 
 defineProps({
     title: String,
@@ -10,7 +11,9 @@ defineProps({
 <template>
     <div class="flex flex-col gap-3 bg-body">
         <div class="flex items-center justify-center w-full h-32 bg-libelo-500 rounded-b-xl">
-            <img src="/img/logotipo.png" alt="Logotipo de Libelo">
+            <div class="w-24 text-white">
+                <LibeloLogotipo />
+            </div>
         </div>
         <div class="flex flex-col px-2 mb-3">
             <h1 class="font-semibold text-2xl">{{ title }}</h1>
