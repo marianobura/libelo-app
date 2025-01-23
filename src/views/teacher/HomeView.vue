fed5a60dc843593017254d8952b2d9356641ff32
marianobura <marianoobura@gmail.com>
2025-01-22 23:23:40 -0300

    Remove unused code and refactor HomeView.vue

--- src/views/teacher/HomeView.vue
+++ src/views/teacher/HomeView.vue
@@ -1,17 +1,7 @@
 <script setup>
 import HomeNav from "@/components/Home/HomeNav.vue";
 import BaseBody from "@/components/BaseBody.vue";
-import { ref, onMounted } from "vue";
-import { useUserStore } from '@/stores/userStore';
 import HomeHeader from "@/components/Home/HomeHeader.vue";
-
-const userStore = useUserStore();
-const loading = ref(true);
-
-onMounted(async () => {
-    await userStore.fetchUser();
-    loading.value = false;
-});
 </script>
 
 <template>
