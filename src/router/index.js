895737515470b1150a8ff1cde7310cd3fa7a9457
marianobura <marianoobura@gmail.com>
2025-01-22 22:11:36 -0300

    Update routes in router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,69 +2,75 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
     {
-        path: "/welcome",
+        path: "/",
         name: "welcome",
-        component: () => import("../views/WelcomeView.vue"),
+        component: () => import("@/views/WelcomeView.vue"),
         meta: { requiresAuth: false },
     },
     {
         path: "/login",
         name: "login",
-        component: () => import("../views/LoginView.vue"),
+        component: () => import("@/views/LoginView.vue"),
         meta: { requiresAuth: false },
     },
     {
         path: "/register",
         name: "register",
-        component: () => import("../views/RegisterView.vue"),
+        component: () => import("@/views/RegisterView.vue"),
         meta: { requiresAuth: false },
     },
     {
-        path: "/",
-        name: "home",
-        component: () => import("../views/HomeView.vue"),
+        path: "/student",
+        name: "student-home",
+        component: () => import("@/views/student/HomeView.vue"),
+        meta: { requiresAuth: true },
+    },
+    {
+        path: "/teacher",
+        name: "teacher-home",
+        component: () => import("@/views/teacher/HomeView.vue"),
         meta: { requiresAuth: true },
     },
     {
         path: "/subject/:id",
         name: "subject",
-        component: () => import("../views/SubjectView.vue"),
+        component: () => import("@/views/student/SubjectView.vue"),
         meta: { requiresAuth: true },
     },
     {
         path: "/subject/:id/chat/fast",
         name: "fast-chat",
-        component: () => import("../views/FastChatView.vue"),
+        component: () => import("@/views/student/FastChatView.vue"),
         meta: { requiresAuth: true },
     },
     {
         path: "/subject/:id/work",
         name: "work",
-        component: () => import("../views/WorkView.vue"),
+        component: () => import("@/views/student/WorkView.vue"),
         meta: { requiresAuth: true },
     },
     {
         path: "/subject/:id/calendar",
         name: "calendar",
-        component: () => import("../views/CalendarView.vue"),
+        component: () => import("@/views/student/CalendarView.vue"),
         meta: { requiresAuth: true },
     },
     {
         path: "/settings",
         name: "settings",
-        component: () => import("../views/SettingsView.vue"),
+        component: () => import("@/views/SettingsView.vue"),
         meta: { requiresAuth: true },
     },
     {
         path: "/settings/edit-profile",
         name: "edit-profile",
-        component: () => import("../views/EditProfileView.vue"),
+        component: () => import("@/views/EditProfileView.vue"),
         meta: { requiresAuth: true },
     },
     {
         path: "/settings/change-password",
         name: "change-password",
-        component: () => import("../views/ChangePasswordView.vue"),
+        component: () => import("@/views/ChangePasswordView.vue"),
         meta: { requiresAuth: true },
     },
 ];
@@ -75,12 +81,17 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-    const isAuthenticated = localStorage.getItem("token");
+    const token = localStorage.getItem("token");
+    const role = localStorage.getItem("role");
 
-    if (to.meta.requiresAuth && !isAuthenticated) {
+    if (to.meta.requiresAuth && !token) {
         next({ name: "welcome" });
-    } else if (!to.meta.requiresAuth && isAuthenticated) {
-        next({ name: "home" });
+    } else if (!to.meta.requiresAuth && token) {
+        if (role === 'student') {
+            next({ name: 'student-home' });
+        } else if (role === 'teacher') {
+            next({ name: 'teacher-home' });
+        }
     } else {
         next();
     }
