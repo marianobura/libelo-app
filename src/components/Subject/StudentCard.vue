d9a95bff7e600a71823e4de73fbd062a2b29a697
marianobura <marianoobura@gmail.com>
2025-01-25 20:40:08 -0300

    Add HomeCard and StudentCard components

---
+++ src/components/Subject/StudentCard.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex items-center justify-between gap-8 w-full p-2 odd:bg-neutral-300 even:bg-neutral-200">
+        <div class="flex gap-2 items-center">
+            <div class="size-10 bg-libelo-500 rounded-full flex-shrink-0" />
+            <div class="flex flex-col">
+                <p class="font-semibold text-sm break-all line-clamp-1">Nombre del alumno</p>
+                <p class="text-neutral-600 text-sm break-all line-clamp-1">Mensaje</p>
+            </div>
+        </div>
+        <p class="text-sm">12:49hs</p>
+    </div>
+</template>
