ceffb9a0f49562ecfc7e6cb2234d28b9741e442a
marianobura <marianoobura@gmail.com>
2025-01-25 20:39:43 -0300

    Remove text-neutral-900 class in BaseTitle and WelcomeView components

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -20,7 +20,7 @@ const handleRoleSelection = (role) => {
         </div>
         <div class="flex flex-col justify-between p-2">
             <div class="flex flex-col items-center gap-2 mb-4">
-                <h1 class="font-semibold text-2xl text-neutral-900">¡Bienvenido a Libelo!</h1>
+                <h1 class="font-semibold text-2xl">¡Bienvenido a Libelo!</h1>
                 <p class="text-neutral-700 text-balance text-center">Estamos encantados de ayudarte a mejorar tu aprendizaje de manera sencilla y eficaz.</p>
             </div>
             <div class="flex flex-col gap-2">
