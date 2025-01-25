ee129b2d068e634f9ad36a95a09498a9855cc79f
marianobura <marianoobura@gmail.com>
2025-01-25 20:40:08 -0300

    Add HomeCard and StudentCard components

---
+++ src/components/Home/HomeCard.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+    content: String,
+});
+</script>
+
+<template>
+    <div class="flex items-center justify-center w-full h-20 p-4 rounded-xl bg-red-800 uppercase">
+        <div class="line-clamp-2 text-center break-words">{{ content }}</div>
+    </div>
+</template>
