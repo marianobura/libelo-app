5995138d9b995e7a3728c60d213825eaefab8cf7
marianobura <marianoobura@gmail.com>
2025-02-02 02:40:15 -0300

    Add ChatInput and ChatView components

---
+++ src/components/Chat/ChatInput.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+import { SendHorizontalIcon } from 'lucide-vue-next';
+
+const props = defineProps({
+    modelValue: String,
+    isSending: Boolean,
+});
+
+const emit = defineEmits(['update:modelValue', 'sendMessage']);
+
+const onInput = (event) => {
+    emit('update:modelValue', event.target.value);
+};
+
+const sendMessage = () => {
+    if (!props.isSending) {
+        emit('sendMessage');
+    }
+};
+</script>
+
+
+<template>
+    <div class="flex items-center gap-3 bg-white rounded-full w-full h-14 p-2">
+        <div class="bg-blue-950 h-full w-full">
+            <input :value="props.modelValue" @input="onInput" type="text" class="w-full h-full text-black pl-2" placeholder="Escribe un mensaje..." />
+        </div>
+        <div @click="sendMessage" :disabled="props.isSending" :class="props.isSending ? 'bg-libelo-700' : 'bg-libelo-500'" class="flex items-center justify-center size-10 rounded-full flex-shrink-0 text-white">
+            <SendHorizontalIcon :size="20" />
+        </div>
+    </div>
+</template>
---
+++ src/views/ChatView.vue
@@ -0,0 +1,116 @@
+<script setup>
+import { ref, onMounted, computed } from 'vue';
+import { useRoute } from 'vue-router';
+import { useUserStore } from '@/stores/userStore';
+import { useSubjectStore } from '@/stores/subjectStore';
+import axios from 'axios';
+import BaseNav from '@/components/BaseNav.vue';
+import BaseBody from '@/components/BaseBody.vue';
+import ChatInput from '@/components/Chat/ChatInput.vue';
+import UserAvatar from '@/components/UserAvatar.vue';
+import { LoaderCircle } from 'lucide-vue-next';
+
+const userStore = useUserStore();
+const subjectStore = useSubjectStore();
+const route = useRoute();
+
+const user = computed(() => userStore.user);
+const subjectId = computed(() => route.params.id);
+
+const messages = ref([]);
+const userMessage = ref('');
+const loading = ref(false);
+const isSending = ref(false);
+
+const fetchChatMessages = async () => {
+    if (!subjectId.value) return;
+
+    loading.value = true;
+    try {
+        const apiUrl = new URL(`/api/chats/${subjectId.value}`, process.env.VUE_APP_API_URL);
+        const response = await axios.get(apiUrl.toString());
+
+        if (response.data?.data?.messages) {
+            messages.value = response.data.data.messages.map(message => ({
+                sender: message.sender,
+                text: message.message
+            }));
+        }
+    } catch (error) {
+        console.error('Error al obtener los mensajes:', error);
+    } finally {
+        loading.value = false;
+    }
+};
+
+const sendMessage = async () => {
+    if (isSending.value || !userMessage.value.trim() || !subjectId.value) return;
+
+    isSending.value = true;
+
+    const newMessage = {
+        subjectId: subjectId.value,
+        studentId: user.value.role === 'student' ? user.value._id : null,
+        teacherId: user.value.role === 'teacher' ? user.value._id : null,
+        message: userMessage.value,
+        sender: user.value._id,
+    };
+
+    try {
+        const apiUrl = new URL(`/api/chats/${subjectId.value}`, process.env.VUE_APP_API_URL);
+        await axios.post(apiUrl.toString(), newMessage);
+
+        messages.value.push({
+            sender: {
+                _id: user.value._id,
+                role: user.value.role,
+                displayName: user.value.displayName,
+            },
+            text: userMessage.value
+        });
+        userMessage.value = '';
+    } catch (error) {
+        console.error('Error al enviar mensaje:', error);
+    } finally {
+        isSending.value = false;
+    }
+};
+
+onMounted(async () => {
+    await userStore.fetchUser();
+    await subjectStore.fetchSubject(subjectId.value);
+    await fetchChatMessages();
+});
+</script>
+
+<template>
+    <BaseBody class="max-h-screen">
+        <BaseNav title="Chat profesional" />
+        <div class="flex flex-col justify-between gap-2 p-2 pt-0 max-h-[calc(100vh-60px)]">
+            <div :class="loading ? 'overflow-hidden h-full' : 'overflow-y-auto'" class="flex flex-col">
+                <div :class="loading ? 'items-center justify-center h-full' : 'justify-end'" class="flex flex-col gap-5 pt-2">
+                    <div v-if="loading" class="flex flex-col gap-2 items-center justify-center w-full text-gray-500">
+                        <LoaderCircle class="animate-spin" size="32"/>
+                        <span class="font-medium">Cargando mensajes...</span>
+                    </div>
+                    <div v-else v-for="(message, index) in messages" :key="index" class="flex gap-2">
+                        <div :class="message.sender.role === 'student' ? 'bg-libelo-500' : 'bg-orange-600'" class="flex items-center justify-center size-10 rounded-full text-white flex-shrink-0">
+                            <UserAvatar :userLetter="message.sender.displayName.charAt(0)" :size="'10'" :orange="message.sender.role === 'teacher'" />
+                        </div>
+                        <div class="flex flex-col w-full gap-1">
+                            <span :class="message.sender.role === 'student' ? 'text-libelo-500' : 'text-orange-600'" class="text-sm font-semibold">
+                                {{ message.sender.displayName }}
+                            </span>
+                            <div :class="message.sender.role === 'student' ? 'bg-libelo-500 text-white' : 'bg-orange-600/40'" class="p-2 rounded-xl w-fit">
+                                <p class="text-sm">{{ message.text }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full">
+                <ChatInput v-model="userMessage" @sendMessage="sendMessage" :isSending="isSending" />
+            </div>
+        </div>
+    </BaseBody>
+</template>
