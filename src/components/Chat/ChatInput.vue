af6b7d7c1856b8299a15b2927d9b95e2f3d7bc59
marianobura <marianoobura@gmail.com>
2025-02-05 21:22:49 -0300

    Update input styles and outline behavior

--- src/components/Chat/ChatInput.vue
+++ src/components/Chat/ChatInput.vue
@@ -22,9 +22,9 @@ const sendMessage = () => {
 
 
 <template>
-    <div class="flex items-center gap-3 bg-white rounded-full w-full h-14 p-2">
+    <div class="flex items-center gap-3 bg-white rounded-full w-full h-14 p-2 outline outline-none has-[input:focus-within]:outline has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-libelo-500">
         <div class="bg-blue-950 h-full w-full">
-            <input :value="props.modelValue" @input="onInput" type="text" class="w-full h-full text-black pl-2" placeholder="Escribe un mensaje..." />
+            <input :value="props.modelValue" @input="onInput" type="text" class="w-full h-full text-black pl-2 focus:outline-none" placeholder="Escribe un mensaje..." />
         </div>
         <div @click="sendMessage" :disabled="props.isSending" :class="props.isSending ? 'bg-libelo-700' : 'bg-libelo-500'" class="flex items-center justify-center size-10 rounded-full flex-shrink-0 text-white">
             <SendHorizontalIcon :size="20" />
