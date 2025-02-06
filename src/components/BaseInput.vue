a2b5d2919416ec22f2b6ece6f53cce4f2efa1573
marianobura <marianoobura@gmail.com>
2025-02-05 21:09:05 -0300

    Update BaseInput.vue styling

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -43,10 +43,9 @@ const togglePasswordVisibility = () => {
             <label class="font-semibold" :class="{ 'text-red-500': error }" :for="identifier">{{ label }}</label>
         </div>
         <div class="w-full" :class="password ? 'flex gap-2' : ''">
-            <input class="w-full text-gray-700 border rounded-xl py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500 pr-10"
+            <input class="w-full text-gray-700 border border-gray-300 outline-1 -outline-offset-1 outline-gray-300 rounded-xl py-3 px-4 leading-tight focus:bg-white focus:outline-libelo-500 pr-10"
                 :class="{
-                    'border-gray-300': !error, 
-                    'bg-red-100 border-red-500': error
+                    'bg-red-100 border-red-500 placeholder:text-neutral-500': error
                 }"
                 :name="identifier" :id="identifier" :type="password ? (isPasswordVisible ? 'text' : 'password') : type" :placeholder="placeholder" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)">
             <button v-if="password" type="button" class="size-[46px] flex items-center justify-center flex-shrink-0 bg-white border border-gray-300 rounded-xl" @click="togglePasswordVisibility">
