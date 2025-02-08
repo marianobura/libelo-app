6bb3b5079727bbe1f11029366d5e766c85422d53
marianobura <marianoobura@gmail.com>
2025-02-07 21:37:12 -0300

    Add props and import UserAvatar component in StudentCard.vue

--- src/components/Subject/StudentCard.vue
+++ src/components/Subject/StudentCard.vue
@@ -1,12 +1,27 @@
+<script setup>
+import { defineProps } from "vue";
+import UserAvatar from "../UserAvatar.vue";
+
+const props = defineProps({
+    student: Object,
+    linkTo: String,
+});
+</script>
+
 <template>
-    <div class="flex items-center justify-between gap-8 w-full p-2 odd:bg-neutral-300 even:bg-neutral-200">
-        <div class="flex gap-2 items-center">
-            <div class="size-10 bg-libelo-500 rounded-full flex-shrink-0" />
+    <router-link :to="props.linkTo" class="flex items-center justify-between gap-8 w-full p-2 odd:bg-neutral-300 even:bg-neutral-200">
+        <div class="flex gap-2 items-center max-w-[70%]">
+            <div>
+                <UserAvatar :userLetter="props.student.name.charAt(0)" :size="'10'" />
+            </div>
             <div class="flex flex-col">
-                <p class="font-semibold text-sm break-all line-clamp-1">Nombre del alumno</p>
-                <p class="text-neutral-600 text-sm break-all line-clamp-1">Mensaje</p>
+                <p class="font-semibold text-sm break-all line-clamp-1">{{ props.student.name }}</p>
+                <p class="text-neutral-600 text-sm break-all line-clamp-1">{{ props.student.message }}</p>
             </div>
         </div>
-        <p class="text-sm">12:49hs</p>
-    </div>
+        <div class="flex flex-col">
+            <p class="text-sm">{{ props.student.date }}</p>
+            <p class="text-sm">{{ props.student.hour }} hs</p>
+        </div>
+    </router-link>
 </template>
