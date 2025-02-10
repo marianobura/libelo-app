9545a3b90eff2bd8fa774d46ffb77d83d0a5d5cd
marianobura <marianoobura@gmail.com>
2025-02-10 02:24:52 -0300

    Refactor SubjectBanner.vue to handle different roles

--- src/components/SubjectBanner.vue
+++ src/components/SubjectBanner.vue
@@ -1,18 +1,32 @@
 <script setup>
 import { computed, watchEffect } from "vue";
+import { useUserStore } from "@/stores/userStore";
 import { useSubjectStore } from "@/stores/subjectStore";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
+const userStore = useUserStore();
 const subjectStore = useSubjectStore();
 
 watchEffect(() => {
     if (route.params.id) {
-        subjectStore.fetchSubject(route.params.id);
+        if (userStore.user.role === 'student') {
+            subjectStore.fetchSubject(route.params.id);
+        } else if (userStore.user.role === 'teacher') {
+            userStore.user?.preferredSubjects[route.params.id];
+        }
     }
 });
 
-const subjectName = computed(() => subjectStore.subjectData?.name || "Materia");
+const subjectName = computed(() => {
+    if (userStore.user.role === 'student') {
+        return subjectStore.subjectData?.name ?? "Materia";
+    } else if (userStore.user.role === 'teacher') {
+        return userStore.user?.preferredSubjects[route.params.id] ?? "Materia";
+    } else {
+        return "Materia";
+    }
+});
 </script>
 
 <template>
