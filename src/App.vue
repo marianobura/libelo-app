36aa5ce93a09d5422a9514e6b93d2f6199955c56
marianobura <marianoobura@gmail.com>
2025-02-13 21:12:54 -0300

    Remove unused import and function call in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <script setup>
 import { onMounted } from 'vue';
 import { useUserStore } from './stores/userStore';
-import { useSubjectStore } from './stores/subjectStore';
 
 const userStore = useUserStore();
-const subjectStore = useSubjectStore();
 
 onMounted(() => {
     userStore.fetchUser();
-    subjectStore.fetchSubject();
 });
 </script>
 
