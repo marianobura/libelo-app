3a193d5fa3b835fa7e503a272e1fcc002ce1a812
Sebastian-Winter12 <sebastian.winter@davinci.edu.ar>
2025-03-07 12:29:49 -0300

    Mejorando diseño

--- src/views/teacher/PointsView.vue
+++ src/views/teacher/PointsView.vue
@@ -2,6 +2,7 @@
 import BaseBody from "@/components/BaseBody.vue";
 import BaseNav from "@/components/BaseNav.vue";
 import promotionsData from "@/assets/promotions.json";
+import pedidosYaImage from "@/assets/promos/pedidosYa.jpg";
 import axios from "axios";
 import { ref, onMounted, computed } from "vue";
 import { useRouter } from "vue-router";
@@ -131,35 +132,41 @@ onMounted(() => {
         <h2 class="font-bold text-xl">Trabaja y gana puntos</h2>
         <p class="text-gray-600">¡Gana puntos trabajando y canjealos en lo que quieras!</p>
       </div>
+    </div>
 
+      <div class="text-white pt-20 pb-8 rounded bg-container bg-no-repeat" :style="{ backgroundImage: `url(${pedidosYaImage})` }">
+        <h2 class="font-bold text-lg mb-2">Promoción destacada</h2>
+        <p class="text-sm mb-2">TE REGALAMOS 5$ EN TU PRIMER PEDIDO</p>
+        <p class="text-sm font-bold">CÓDIGO: PY-8C51B4E</p>
+      </div>
+    
+    <div class="p-4">
       <div>
         <h2 class="font-bold text-lg mb-2">Promociones destacadas</h2>
 
         <div v-if="loading" class="text-center">Cargando promociones...</div>
 
         <div v-else>
-        <div v-if="promotionsWithRandomSelection.length">
-          <div v-for="category in promotions" :key="category.category" class="mb-6">
-            <div class="overflow-hidden w-full">
-              <div class="overflow-x-auto no-scrollbar snap-x snap-mandatory scroll-smooth touch-pan-x flex space-x-3 py-2 px-4" ref="slider">
-                <div v-for="(promotion, index) in promotionsWithRandomSelection.flatMap(category => category.promotions)" :key="index" class="snap-center flex-none w-3/4 sm:w-1/2 md:w-1/3 lg:w-1/4 p-2 rounded text-center bg-gray-100 shadow-md cursor-pointer hover:bg-gray-200" @click="goToPromotion(promotion.id, promotion.category || 'default-category')">
-                  <img :src="promotion.image" alt="Imagen de promoción" class="w-full h-40 object-cover rounded">
-                  <p class="text-sm font-bold">{{ promotion.category }}</p>
-                  <h3 class="text-lg mt-2">{{ promotion.title }}</h3>
-                  <p class="text-sm font-bold">{{ promotion.description }}</p>
-                  <p class="text-sm">Ubicación: {{ promotion.location }}</p>
-                  <p class="text-sm">Válido hasta: {{ promotion.valid_until }}</p>
-                  <p class="text-sm">{{ promotion.terms }}</p>
-                  <p class="text-sm font-bold">Puntos {{ promotion.points }}</p>
+          <div v-if="promotionsWithRandomSelection.length">
+            <div v-for="category in promotions" :key="category.category" class="mb-6">
+              <div class="overflow-hidden w-full">
+                <div class="overflow-x-auto no-scrollbar snap-x snap-mandatory scroll-smooth touch-pan-x flex space-x-3 py-2 px-4" ref="slider">
+                  <div v-for="(promotion, index) in promotionsWithRandomSelection.flatMap(category => category.promotions)" :key="index" class="snap-center flex-none w-3/4 sm:w-1/2 md:w-1/3 lg:w-1/4 p-2 rounded text-center bg-gray-100 shadow-md cursor-pointer hover:bg-gray-200" @click="goToPromotion(promotion.id, promotion.category || 'default-category')">
+                    <img :src="promotion.image" alt="Imagen de promoción" class="w-full h-40 object-cover rounded">
+                    <p class="text-sm font-bold">{{ promotion.category }}</p>
+                    <h3 class="text-lg mt-2">{{ promotion.title }}</h3>
+                    <p class="text-sm font-bold">{{ promotion.description }}</p>
+                    <p class="text-sm">Ubicación: {{ promotion.location }}</p>
+                    <p class="text-sm">Válido hasta: {{ promotion.valid_until }}</p>
+                    <p class="text-sm">{{ promotion.terms }}</p>
+                    <p class="text-sm font-bold">Puntos {{ promotion.points }}</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-
-        
       </div>
-    </div>
 
       <div class="p-4">
         <h2 class="font-bold text-lg mb-2">Lista de promociones</h2>
