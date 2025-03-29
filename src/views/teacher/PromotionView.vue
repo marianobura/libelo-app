9f09d75a0cdea1e1107d82038310b55caeec30f9
marianobura <marianoobura@gmail.com>
2025-03-28 23:36:10 -0300

    Refactor PromotionView UI design

--- src/views/teacher/PromotionView.vue
+++ src/views/teacher/PromotionView.vue
@@ -7,6 +7,7 @@ import { ref, onMounted, computed } from "vue";
 import { useRoute } from "vue-router";
 import { useUserStore } from "@/stores/userStore";
 import axios from "axios";
+import { Coins, Info, MapPin, TicketCheck } from "lucide-vue-next";
 
 const promotions = ref(promotionsData.promotions);
 const route = useRoute();
@@ -71,25 +72,50 @@ const redeemPromotion = async () => {
         <BaseNav title="Promoción" />
         <div class="p-4">
             <div v-if="!promotion.title" class="text-center">Promoción no encontrada.</div>
-            <div v-else>
-                <img :src="promotion.image" alt="Promoción" class="w-full h-auto object-cover rounded mb-4">
-                <h3 class="font-bold text-lg mb-2">{{ promotion.title }}</h3>
-                <p class="text-sm">{{ promotion.description }}</p>
-                <p class="text-sm">Ubicación: {{ promotion.location }}</p>
-                <p class="text-sm">Válido hasta: {{ promotion.valid_until }}</p>
-                <p class="text-sm font-bold">Términos: {{ promotion.terms }}</p>
-                <p class="text-sm font-bold">Mis puntos: {{ userPoints }}</p>
-
-                <BaseButton @click="openModal" class="mt-4" primary>
-                    Canjear ({{ promotion.points }} Puntos)
-                </BaseButton>
+            <div v-else class="flex flex-col justify-between h-full gap-4">
+                <div>
+                    <img :src="promotion.image" alt="Promoción" class="w-full h-56 object-cover rounded">
+                    <div class="flex flex-col py-2 border-b border-neutral-300">
+                        <div class="flex justify-between items-center">
+                            <h3 class="font-bold overflow-hidden text-ellipsis line-clamp-1">{{ promotion.title }}</h3>
+                            <div class="flex gap-1 items-center bg-libelo-500/10 text-libelo-500 px-2 rounded">
+                                <Coins size="16" />
+                                <span class="font-semibold">{{ promotion.points }}</span>
+                            </div>
+                        </div>
+                        <p v-if="promotion.name !== 'Genérica'" class="text-sm">Promoción por <span class="font-semibold">{{ promotion.name }}</span></p>
+                    </div>
+                    <div class="mt-2 flex flex-col gap-2">
+                        <p class="font-semibold">{{ promotion.description }}</p>
+                        <div class="flex gap-2 items-center">
+                            <div class="p-1 rounded text-libelo-500 bg-libelo-500/10 mb-auto">
+                                <MapPin size="20" />
+                            </div>
+                            <p>{{ promotion.location }}</p>
+                        </div>
+                        <div class="flex gap-2 items-center">
+                            <div class="p-1 rounded text-libelo-500 bg-libelo-500/10 mb-auto">
+                                <TicketCheck size="20" />
+                            </div>
+                            <p>Válido hasta {{ promotion.valid_until }}</p>
+                        </div>
+                        <div class="flex gap-2 items-center">
+                            <div class="p-1 rounded text-libelo-500 bg-libelo-500/10 mb-auto">
+                                <Info size="20" />
+                            </div>
+                            <p>{{ promotion.terms }}</p>
+                        </div>
+                    </div>
+                </div>
+                <BaseButton v-if="userPoints >= promotion.points" @click="openModal" primary>Canjear por {{ promotion.points }} puntos</BaseButton>
+                <BaseButton v-else danger>No tienes puntos suficientes</BaseButton>
             </div>
         </div>
 
         <div v-if="showModal" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
             <div class="bg-white p-4 rounded shadow-lg w-80">
                 <h3 class="font-bold text-lg mb-2">Canjear promoción</h3>
-                <p class="text-sm">¿Estás seguro de que deseas canjear esta promoción?</p>
+                <p>¿Estás seguro de que deseas canjear esta promoción?</p>
                 <p v-if="errors.promo" class="text-red-500 text-sm">{{ errors.promo }}</p>
                 <div class="flex justify-end mt-4 gap-2">
                     <BaseButton @click="closeModal" danger>Cancelar</BaseButton>
