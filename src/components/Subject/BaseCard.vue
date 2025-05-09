16952b9135783d5856fbf608dd9286cae562bfa2
marianobura <marianoobura@gmail.com>
2025-05-09 18:36:17 -0300

    Refactor BaseCard component to include items center alignment

--- src/components/Subject/BaseCard.vue
+++ src/components/Subject/BaseCard.vue
@@ -16,7 +16,7 @@ const props = defineProps({
 <template>
     <router-link :to="props.linkTo">
         <div class="flex flex-col p-3 h-fit rounded-xl text-neutral-700" :class="props.color ? 'bg-gradient-to-b from-[#D8D8D8] to-[#CECECE]' : 'border border-neutral-300'">
-            <div class="flex gap-2">
+            <div class="flex gap-2 items-center">
                 <component :is="props.icon" :size="20" />
                 <p class="font-bold">{{ props.title }}</p>
             </div>
