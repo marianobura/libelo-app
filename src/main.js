74a412d49a6a0e4b9285023d6889ee211d2a55c3
marianobura <marianoobura@gmail.com>
2025-05-09 18:36:05 -0300

    Modified Google Sign-In functionality

--- src/main.js
+++ src/main.js
@@ -5,21 +5,11 @@ import "./style.css";
 import router from "./router";
 import { createPinia } from "pinia";
 import "@/services/axios";
-import vue3GoogleLogin from "vue3-google-login";
-
-const CLIENT_ID = process.env.VUE_APP_GOOGLE_CLIENT_ID;
 
 const app = createApp(App);
-
 const pinia = createPinia();
 
 app.use(pinia);
 app.use(router);
-app.use(vue3GoogleLogin, {
-    clientId: CLIENT_ID,
-    scope: "openid email profile calendar.readonly classroom.courses.readonly",
-    prompt: "consent",
-    loginHint: "",
-});
 
 app.mount("#app");
