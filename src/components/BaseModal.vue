75e1a6f0bdc4075aa3998eb89f7f4cb677f1772a
marianobura <marianoobura@gmail.com>
2025-05-10 18:37:03 -0300

    Add overflow control to BaseModal component

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,3 +1,15 @@
+<script setup>
+import { onMounted, onUnmounted } from 'vue'
+
+onMounted(() => {
+    document.body.classList.add('overflow-hidden')
+})
+
+onUnmounted(() => {
+    document.body.classList.remove('overflow-hidden')
+})
+</script>
+
 <template>
     <div class="fixed inset-0 flex bg-black/20 z-50">
         <slot />
