428630444e290f2ae57b96f3595eac470bc9ab77
marianobura <marianoobura@gmail.com>
2025-05-17 20:38:31 -0300

    Add disabled state handling to BaseButton component

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -18,10 +18,15 @@ const props = defineProps({
     danger: {
         type: Boolean,
         default: false
+    },
+    disabled: {
+        type: Boolean,
+        default: false
     }
 });
 
 const handleClick = () => {
+    if (props.disabled) return;
     if (props.action) {
         props.action();
     } else if (props.path) {
@@ -31,12 +36,14 @@ const handleClick = () => {
 </script>
 
 <template>
-    <button @click="handleClick" 
+    <button @click="handleClick"
+        :disabled="props.disabled"
         class="w-full h-12 rounded-xl font-semibold"
         :class="{
             'bg-libelo-500 text-white hover:bg-libelo-600': props.primary && !props.danger,
             'bg-neutral-200 text-black border border-neutral-300 hover:bg-neutral-300': !props.primary && !props.danger,
-            'bg-red-700 text-white hover:bg-red-800': props.danger
+            'bg-red-700 text-white hover:bg-red-800': props.danger,
+            'bg-libelo-500 opacity-60 cursor-not-allowed': props.disabled && props.primary,
         }">
         <slot />
     </button>
