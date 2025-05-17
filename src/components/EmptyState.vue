5fff4b07b97bf5f621180e032592dc941ef9566a
marianobura <marianoobura@gmail.com>
2025-05-17 20:39:02 -0300

    Add BookX icon to EmptyState component

--- src/components/EmptyState.vue
+++ src/components/EmptyState.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { defineProps, computed } from 'vue';
-import { BellOff, MailX } from 'lucide-vue-next';
+import { BellOff, MailX, BookX } from 'lucide-vue-next';
 
 const props = defineProps({
     title: String,
@@ -8,13 +8,13 @@ const props = defineProps({
     icon: String,
 });
 
-const icons = { BellOff, MailX };
+const icons = { BellOff, MailX, BookX };
 
 const SelectedIcon = computed(() => icons[props.icon]);
 </script>
 
 <template>
-    <div class="flex flex-col gap-4 items-center justify-center w-full text-neutral-800">
+    <div class="flex flex-col gap-2 items-center justify-center w-full text-neutral-800">
         <component :is="SelectedIcon" size="64" />
         <div class="flex flex-col">
             <span class="text-center text-xl font-medium text-700 border-b border-neutral-300 pb-2">{{ title }}</span>
