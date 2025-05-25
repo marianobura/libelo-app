afd212f1c1c9bf60eeacf027f2237ad13c4be11f
marianobura <marianoobura@gmail.com>
2025-05-24 23:10:33 -0300

    Refactor BaseCard.vue to add 'disabled' prop and improve link handling

--- src/components/Subject/BaseCard.vue
+++ src/components/Subject/BaseCard.vue
@@ -4,23 +4,29 @@ import { defineProps } from 'vue';
 const props = defineProps({
     title: String,
     description: String,
-    color: {
-        type: Boolean,
-        default: true
-    },
+    chat: Boolean,
     icon: Function,
-    linkTo: String
+    linkTo: String,
+    disabled: {
+        type: Boolean,
+        default: false
+    }
 });
 </script>
 
 <template>
-    <router-link :to="props.linkTo">
-        <div class="flex flex-col p-3 h-fit rounded-xl text-neutral-700" :class="props.color ? 'bg-gradient-to-b from-[#D8D8D8] to-[#CECECE]' : 'border border-neutral-300'">
-            <div class="flex gap-2 items-center">
-                <component :is="props.icon" :size="20" />
-                <p class="font-bold">{{ props.title }}</p>
-            </div>
-            <p class="text-sm">{{ props.description }}</p>
+    <component
+        :is="!props.disabled && props.linkTo ? 'router-link' : 'div'"
+        :to="!props.disabled ? props.linkTo : null"
+        class="flex flex-col p-3 h-fit rounded-xl text-neutral-700 transition duration-200"
+        :class="[
+            props.chat ? 'bg-gradient-to-b from-[#D8D8D8] to-[#CECECE]' : 'border border-neutral-300',
+            props.disabled ? 'opacity-60 cursor-not-allowed bg-neutral-300' : 'hover:bg-neutral-200',
+        ]">
+        <div class="flex gap-2 items-center">
+            <component :is="props.icon" :size="20" />
+            <p class="font-bold">{{ props.title }}</p>
         </div>
-    </router-link>
+        <p class="text-sm">{{ props.description }}</p>
+    </component>
 </template>
