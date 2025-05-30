cfa8205cffe0a426bb7e8fd4973dc85b952bc8af
marianobura <marianoobura@gmail.com>
2025-05-29 22:01:19 -0300

    Add reset password functionality and improve UI consistency in sign-in components. [Sebastian-Winter12]

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -130,7 +130,7 @@ const handleTokenFromGoogle = async (accessToken) => {
         <BaseBody sign>
             <div class="flex flex-col gap-3">
                 <BaseInput identifier="email" placeholder="usuario@email.com" label="Correo electrónico" type="email" v-model="email" :error="!!errors.email" :error-message="errors.email" />
-                <BaseInput password identifier="password" placeholder="Ingresa tu contraseña" label="Contraseña" type="password" v-model="password" :error="!!errors.password" :error-message="errors.password" />
+                <BaseInput password reset identifier="password" placeholder="Ingresa tu contraseña" label="Contraseña" type="password" v-model="password" :error="!!errors.password" :error-message="errors.password" />
                 <div v-if="errorMessage" class="flex items-center gap-2 bg-red-100 border border-red-500 text-red-600 p-2 rounded-xl">
                     <CircleAlert :size="16" class="flex-shrink-0 mb-auto mt-0.5" />
                     <span class="text-sm">{{ errorMessage }}</span>
