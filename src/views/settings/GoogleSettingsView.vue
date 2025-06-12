534b922ade2c3cd5d3b77cc8083daa3bba8a0103
marianobura <marianoobura@gmail.com>
2025-06-12 19:57:31 -0300

    Enhance GoogleSettingsView with BaseTitle components for better user feedback on account linkage status.

--- src/views/settings/GoogleSettingsView.vue
+++ src/views/settings/GoogleSettingsView.vue
@@ -5,6 +5,7 @@ import GoogleLogin from '@/components/SignAccount/GoogleLogin.vue';
 import axios from 'axios';
 import { onMounted } from 'vue';
 import { useUserStore } from '@/stores/userStore';
+import BaseTitle from '@/components/BaseTitle.vue';
 
 const userStore = useUserStore();
 
@@ -30,11 +31,18 @@ onMounted(async () => {
     <BaseBody>
         <BaseNav title="Google" />
         <div class="flex flex-col gap-4 p-2">
-            <div v-if="userStore.user.google.isGoogleLinked" class="flex flex-col gap-2">
-                <p>Esta cuenta está asociada a {{ userStore.user.google.gmail }}</p>
+            <div v-if="userStore.user.google.isGoogleLinked" class="flex flex-col gap-4">
+                <BaseTitle title="Cuenta vinculada" description="Esta cuenta se encuentra actualmente asociada a la siguiente dirección de correo electrónico de Google:">
+                    <div class="border border-neutral-300 bg-neutral-200 py-2 px-3 rounded-xl">
+                        <span class="font-semibold text-libelo">{{ userStore.user.google.gmail }}</span>
+                    </div>
+                </BaseTitle>
+                <BaseTitle title="Vincular a otra cuenta" description="Para actualizar o cambiar la vinculación, haga clic en el botón a continuación:">
+                </BaseTitle>
             </div>
-            <div v-else class="flex flex-col gap-2">
-                <p>Esta cuenta no está asociada a ninguna cuenta de Google.</p>
+            <div v-else>
+                <BaseTitle title="Cuenta vinculada" description="Esta cuenta no se encuentra vinculada a una cuenta de Google. Para realizar la vinculación, haz clic en el botón a continuación:">
+                </BaseTitle>
             </div>
             <GoogleLogin :onTokenReceived="handleTokenFromGoogle" secondary>{{ userStore.user.google.isGoogleLinked ? 'Vincular a otra cuenta' : 'Vincular con Google' }}</GoogleLogin>
         </div>
