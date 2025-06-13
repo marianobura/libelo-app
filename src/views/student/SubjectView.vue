cf511d6f9060c154e899e27f46b93655f8d595fe
marianobura <marianoobura@gmail.com>
2025-06-12 22:58:39 -0300

    Refactor BaseCard component usage and update styles in SubjectView

--- src/views/student/SubjectView.vue
+++ src/views/student/SubjectView.vue
@@ -31,18 +31,18 @@ onMounted(async () => {
             <SubjectBanner />
             <BaseTitle title="Opciones de chat" description="Elige entre asistencia inmediata de IA o asesoramiento experto de mentores.">
                 <div class="grid grid-cols-2 gap-2">
-                    <BaseCard :link-to="`/student/subject/${path}/chat/fast`" title="Rápido" description="Realiza una consulta instantánea." :icon="Bot" chat />
-                    <BaseCard :link-to="`/subject/${path}/chat`" title="Profesional" description="Habla con un experto y resuelve tus dudas." :icon="UserRound" chat />
+                    <BaseCard :link-to="`/student/subject/${path}/chat/fast`" description="Realiza una consulta instantánea." :icon="Bot" chat />
+                    <BaseCard :link-to="`/subject/${path}/chat`" description="Habla con un experto y resuelve tus dudas." :icon="UserRound" chat />
                 </div>
             </BaseTitle>
 
             <BaseTitle title="Progreso y planificación" description="Gestiona tus sesiones, evalúa tu avance y mejora con ejercicios prácticos.">
                 <div class="grid grid-cols-2 gap-2">
-                    <BaseCard :link-to="`/student/subject/${path}/objectives`" title="Objetivos" description="Establece y sigue tus metas de aprendizaje." :icon="Goal" />
-                    <BaseCard :link-to="`/student/subject/${path}/calendar`" title="Calendario" description="Planifica y agenda tus próximas sesiones." :icon="CalendarDays" />
-                    <BaseCard v-if="subjectStore.subject?.classroomName" :link-to="`/student/subject/${path}/work`" title="Classroom" description="Revisa y evalúa tu progreso académico." :icon="BookOpenCheck" />
-                    <BaseCard v-else title="Classroom" description="Revisa y evalúa tu progreso académico." :icon="BookOpenCheck" @click="showModalCourses = true" />
-                    <BaseCard title="Desafíos" description="Practica con ejercicios y mejora tus habilidades." :icon="NotebookPen" disabled />
+                    <BaseCard :link-to="`/student/subject/${path}/objectives`" description="Establece y sigue tus metas de aprendizaje." :icon="Goal" />
+                    <BaseCard :link-to="`/student/subject/${path}/calendar`" description="Planifica y agenda tus próximas sesiones." :icon="CalendarDays" />
+                    <BaseCard v-if="subjectStore.subject?.classroomName" :link-to="`/student/subject/${path}/work`" description="Revisa y evalúa tu progreso académico." :icon="BookOpenCheck" />
+                    <BaseCard v-else description="Revisa y evalúa tu progreso académico." :icon="BookOpenCheck" @click="showModalCourses = true" />
+                    <BaseCard description="Practica con ejercicios y mejora tus habilidades." :icon="NotebookPen" disabled />
                 </div>
             </BaseTitle>
 
