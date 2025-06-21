ca535382d50dabd69cf8c7c5583531f0bc5461fc
marianobura <marianoobura@gmail.com>
2025-06-20 23:36:11 -0300

    Add calendar functionality (Sebastian-Winter12)

---
+++ src/components/Calendar/DeleteEventModal.vue
@@ -0,0 +1,46 @@
+<!-- DeleteEventModal.vue -->
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+import BaseModal from '@/components/BaseModal.vue';
+import BaseButton from '@/components/BaseButton.vue';
+
+const props = defineProps({
+    show: Boolean,
+    event: Object
+});
+
+const emit = defineEmits(['close', 'confirm']);
+
+const closeModal = () => {
+    emit("close");
+};
+
+const handleOverlayClick = (event) => {
+    if (event.target === event.currentTarget) {
+        closeModal();
+    }
+};
+
+const handleConfirm = () => emit('confirm');
+</script>
+
+<template>
+    <BaseModal :show="props.show" class="items-center justify-center" @click.self="handleOverlayClick">
+        <div class="bg-white p-6 rounded-xl max-w-md w-full mx-2 max-h-[80vh] overflow-y-auto">
+            <h2 class="text-lg font-bold mb-2">¿Estás seguro de eliminar este evento?</h2>
+            <p class="text-sm text-gray-600">Si lo eliminás, se borrará definitivamente.</p>
+            <p class="text-sm text-gray-800 font-medium mt-2 whitespace-pre-wrap break-words">
+                Evento: <span class="font-semibold">{{ props.event?.summary || 'Sin título' }}</span><br />
+                Fecha:
+                <span class="text-gray-700">
+                    {{ new Date(props.event?.start.dateTime).toLocaleString("es-AR") }} a
+                    {{ new Date(props.event?.end.dateTime).toLocaleString("es-AR") }}
+                </span>
+            </p>
+            <div class="flex justify-end mt-4 gap-2">
+                <BaseButton @click="handleClose" secondary>Cancelar</BaseButton>
+                <BaseButton @click="handleConfirm" danger>Eliminar</BaseButton>
+            </div>
+        </div>
+    </BaseModal>
+</template>
--- src/views/student/CalendarView.vue
+++ src/views/student/CalendarView.vue
@@ -1,56 +1,109 @@
 <script setup>
+/* eslint-disable */
 import 'flowbite/dist/flowbite.min.js';
 import 'flowbite-datepicker';
-
+import { useCalendar } from '@/composables/useCalendar';
 import { ref, computed, onMounted, watch } from "vue";
 import BaseBody from "@/components/BaseBody.vue";
 import BaseNav from "@/components/BaseNav.vue";
 import BaseTitle from "@/components/BaseTitle.vue";
 import SubjectBanner from "@/components/SubjectBanner.vue";
 import { useUserStore } from "@/stores/userStore";
+import { Trash2, ChevronLeft, ChevronRight, CalendarPlus } from "lucide-vue-next";
+import EditModal from "@/components/Calendar/EditModal.vue";
+import DeleteEventModal from "@/components/Calendar/DeleteEventModal.vue";
+import BaseInput from '@/components/BaseInput.vue';
+
+const {
+    currentDate,
+    selectedDays,
+    calendarEvents,
+    changeMonth,
+    toggleDay,
+    selectCurrentDay,
+    isToday,
+    hasEventOnDay,
+    getEventsforSelectedDays
+} = useCalendar();
 
 const userStore = useUserStore();
-const calendarEvents = ref([]);
-const selectedDays = ref([]);
-const currentDate = ref(new Date());
-
+const showEventModal = ref(false);
+const deleteEventModal = ref(false);
+const eventToDelete = ref(null);
+const selectedEvent = ref(null);
+const isEditing = ref(false);
+const formattedEditDate = ref("");
 const currentMonth = computed(() => currentDate.value.getMonth());
 const currentYear = computed(() => currentDate.value.getFullYear());
+const formattedDate = ref('');
 
 const daysInMonth = computed(() => {
-    return new Date(currentYear.value, currentMonth.value + 1, 0).getDate();
+    return Number(new Date(currentYear.value, currentMonth.value + 1, 0).getDate()) || 0;
 });
 
 const firstDayOfMonth = computed(() => {
-    return new Date(currentYear.value, currentMonth.value, 1).getDay();
+    return Number(new Date(currentYear.value, currentMonth.value, 1).getDay()) || 0;
 });
 
-function changeMonth(offset) {
-    const newDate = new Date(currentYear.value, currentMonth.value + offset, 1);
-    currentDate.value = newDate;
-}
 
-function toggleDay(day) {
-    if (selectedDays.value[0] === day) {
-        selectedDays.value = [];
-    } else {
-        selectedDays.value = [day];
-    }
-}
-
-function getEventsforSelectedDays() {
-    if (selectedDays.value.length === 0) return [];
-
-    return calendarEvents.value.filter(event => {
-        const eventDate = new Date(event.start.dateTime || event.start.date);
-        return (
-            eventDate.getFullYear() === currentYear.value &&
-            eventDate.getMonth() === currentMonth.value &&
-            selectedDays.value.includes(eventDate.getDate())
-        );
+// Formatea la fecha y hora de un objeto Date o de un objeto con propiedades dateTime o date
+function formatDateTime(dateObj) {
+    if (!dateObj) return '';
+    const dateStr = dateObj.dateTime || dateObj.date;
+    if (!dateStr) return '';
+    const d = new Date(dateStr);
+    if (isNaN(d)) return '';
+    return d.toLocaleString('es-AR', {
+        hour: '2-digit',
+        minute: '2-digit',
     });
 }
 
+// Confirma la eliminación de un evento con un modal
+function confirmDelete(event) {
+    eventToDelete.value = event;
+    deleteEventModal.value = true;
+}
+
+// Maneja la actualización de un evento desde el modal
+function handleUpdate({ event, date }) {
+    selectedEvent.value = event;
+    formattedEditDate.value = date;
+    saveChanges();
+}
+
+// Maneja la eliminación de un evento desde el modal
+function handleDelete() {
+    eventToDelete.value = selectedEvent.value;
+    deleteEventModal.value = true;
+}
+
+// Guarda los cambios realizados en el evento
+async function saveChanges() {
+    const updatedEvent = {
+        summary: selectedEvent.value.summary,
+        description: selectedEvent.value.description,
+        start: { dateTime: new Date(formattedEditDate.value).toISOString() },
+        end: { dateTime: new Date(new Date(formattedEditDate.value).getTime() + 60 * 60 * 1000).toISOString() }
+    };
+
+    if (!selectedEvent.value.id) {
+        await addEvent(updatedEvent);
+    } else {
+        await editEvent(selectedEvent.value.id, updatedEvent);
+    }
+
+    isEditing.value = false;
+    showEventModal.value = false;
+    await getCalendarEvents();
+}
+
+function openEventModal(event) {
+    selectedEvent.value = event;
+    showEventModal.value = true;
+}
+
+// fetch a la api para trar eventos del calendario
 async function getCalendarEvents() {
     try {
         const start = new Date(currentYear.value, currentMonth.value, 1).toISOString();
@@ -71,13 +124,153 @@ async function getCalendarEvents() {
     }
 }
 
+
+// Elimina evento confirmado
+async function deleteConfirmedEvent() {
+    if (!eventToDelete.value) return;
+
+    try {
+        const url = `https://www.googleapis.com/calendar/v3/calendars/primary/events/${eventToDelete.value.id}`;
+        await fetch(url, {
+            method: "DELETE",
+            headers: {
+                Authorization: `Bearer ${userStore?.user.google.accessToken}`
+            }
+        });
+
+        calendarEvents.value = calendarEvents.value.filter(event => event.id !== eventToDelete.value.id);
+        deleteEventModal.value = false;
+        eventToDelete.value = null;
+    } catch (error) {
+        console.error("Error al eliminar el evento:", error);
+    }
+}
+
+// Edita un evento específico
+async function editEvent(eventId, updatedEvent) {
+    try {
+        const url = `https://www.googleapis.com/calendar/v3/calendars/primary/events/${eventId}`;
+
+        const response = await fetch(url, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${userStore?.user.google.accessToken}`
+            },
+            body: JSON.stringify(updatedEvent)
+        });
+
+        if (!response.ok) {
+            throw new Error("Error al editar el evento");
+        }
+
+        const index = calendarEvents.value.findIndex(event => event.id === eventId);
+        if (index !== -1) {
+            calendarEvents.value[index] = { ...calendarEvents.value[index], ...updatedEvent };
+        }
+    } catch (error) {
+        console.error("Error al editar el evento:", error);
+    }
+}
+
+
+// Agrega un nuevo evento al calendario
+async function addEvent(event) {
+    try {
+        const url = `https://www.googleapis.com/calendar/v3/calendars/primary/events`;
+
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${userStore?.user.google.accessToken}`
+            },
+            body: JSON.stringify(event)
+        });
+
+        if (!response.ok) {
+            throw new Error("Error al agregar el evento");
+        }
+
+        const newEvent = await response.json();
+        calendarEvents.value.push(newEvent);
+    } catch (error) {
+        console.error("Error al agregar el evento:", error);
+    }
+}
+
+// Abre el modal para crear un nuevo evento
+function openNewEventModal() {
+    const selectedDay = selectedDays.value[0];
+    if (!selectedDay) return;
+
+    const startDate = new Date(currentYear.value, currentMonth.value, selectedDay, 12);
+    const endDate = new Date(startDate.getTime() + 60 * 60 * 1000);
+
+    selectedEvent.value = {
+        summary: "Nuevo evento",
+        description: "",
+        start: { dateTime: startDate.toISOString() },
+        end: { dateTime: endDate.toISOString() }
+    };
+
+    formattedEditDate.value = startDate.toISOString().slice(0, 16);
+    isEditing.value = true;
+    showEventModal.value = true;
+}
+
+function formatDateInput(event) {
+    let value = event.target.value.replace(/\D/g, '');
+    if (value.length > 8) value = value.slice(0, 8);
+
+    if (value.length >= 5) {
+        value = `${value.slice(0, 2)}/${value.slice(2, 4)}/${value.slice(4)}`;
+    } else if (value.length >= 3) {
+        value = `${value.slice(0, 2)}/${value.slice(2)}`;
+    }
+
+    formattedDate.value = value;
+
+    if (value.length === 10) {
+        const [dd, mm, yyyy] = value.split('/');
+        const date = new Date(+yyyy, +mm - 1, +dd);
+        if (!isNaN(date)) {
+            currentDate.value = date;
+        }
+    }
+}
+
+const selectedDateLabel = computed(() => {
+    if (!selectedDays.value.length) return '';
+
+    const day = selectedDays.value[0];
+    const date = new Date(currentYear.value, currentMonth.value, day);
+
+    const dd = String(date.getDate()).padStart(2, '0');
+    const mm = String(date.getMonth() + 1).padStart(2, '0');
+    const yyyy = date.getFullYear();
+
+    return `${dd}/${mm}/${yyyy}`;
+});
+
 onMounted(async () => {
     await userStore.fetchUser();
     await getCalendarEvents();
+    await selectCurrentDay();
 });
 
-// Re-fetch when date changes
-watch(currentDate, getCalendarEvents);
+// Llena el input del modal con la fecha exacta
+watch(() => selectedEvent.value, (event) => {
+    if (event?.start?.dateTime) {
+        const date = new Date(event.start.dateTime);
+        formattedEditDate.value = date.toISOString().slice(0, 16);
+    }
+});
+
+watch(currentDate, () => {
+    getCalendarEvents();
+    selectCurrentDay();
+});
 </script>
 
 <template>
@@ -87,32 +280,23 @@ watch(currentDate, getCalendarEvents);
             <SubjectBanner />
             <BaseTitle title="Revisa tus próximas fechas"
                 description="Consulta, organiza y planifica tus próximas fechas de forma simple y eficiente en un solo lugar.">
-
-                <div class="relative">
-                    <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path
-                                d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                        </svg>
-                    </div>
-                    <input datepicker id="default-datepicker" type="text"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-libelo-500 focus:border-libelo-500 block w-full ps-10 p-2.5"
-                        placeholder="Select date">
+                <div class="flex gap-2">
+                    <BaseInput calendar v-model="formattedDate" identifier="custom-datepicker" placeholder="dd/mm/aaaa" @input="formatDateInput" />
+                    <button @click="openNewEventModal" class="flex items-center justify-center bg-libelo-500 rounded-xl text-white size-11 shrink-0">
+                        <CalendarPlus :size="20" />
+                    </button>
+                </div>
+                <div class="flex justify-between items-center p-2 bg-white rounded-xl">
+                    <button @click="changeMonth(-1)" class="size-8 flex items-center justify-center rounded-xl hover:bg-neutral-200">
+                        <ChevronLeft />
+                    </button>
+                    <h2 class="text-xl font-semibold">{{ currentDate.toLocaleDateString("es-AR", { month: "long", year: "numeric"}) }}</h2>
+                    <button @click="changeMonth(1)" class="size-8 flex items-center justify-center rounded-xl hover:bg-neutral-200">
+                        <ChevronRight />
+                    </button>
                 </div>
 
-                <!-- Cabecera del mes y año -->
-                <div class="flex justify-between items-center mb-4">
-                    <button @click="changeMonth(-1)" class="text-gray-500 hover:text-gray-700">&lt;</button>
-                    <h2 class="text-xl font-semibold">
-                        {{ currentDate.toLocaleDateString("es-AR", { month: "long", year: "numeric" }) }}
-                    </h2>
-                    <button @click="changeMonth(1)" class="text-gray-500 hover:text-gray-700">&gt;</button>
-                </div>
-
-                <!-- Días de la semana -->
-                <div
-                    class="grid grid-cols-7 text-center text-gray-600 font-semibold border-b border-gray-300 pb-2 mb-2">
+                <div class="grid grid-cols-7 text-center font-semibold border-b border-gray-300 pb-2 mb-2">
                     <div>Dom</div>
                     <div>Lun</div>
                     <div>Mar</div>
@@ -122,25 +306,26 @@ watch(currentDate, getCalendarEvents);
                     <div>Sáb</div>
                 </div>
 
-                <!-- Calendario dinámico -->
-                <div class="grid grid-cols-7 gap-2 text-center justify-items-center">
+                <div class="grid grid-cols-7 gap-2 text-center justify-items-center border-b border-gray-300 pb-2">
                     <div v-for="n in firstDayOfMonth" :key="'blank-' + n"></div>
-                    <div v-for="day in daysInMonth" :key="day" @click="toggleDay(day)"
-                        class="cursor-pointer rounded-full size-8 flex items-center justify-center select-none transition-colors"
-                        :class="selectedDays.includes(day) ? 'bg-libelo-500 text-white font-semibold shadow' : 'hover:bg-libelo-100'">
+                    <div v-for="day in daysInMonth" :key="day" @click="toggleDay(day)" class="cursor-pointer relative rounded-full size-8 flex items-center justify-center select-none transition-colors" :class="[ selectedDays.includes(day) ? 'bg-libelo-500 text-white font-semibold shadow' : isToday(day) ? 'text-libelo-500 font-bold' : 'hover:bg-libelo-100']">
                         {{ day }}
+                        <span v-if="hasEventOnDay(day) && !selectedDays.includes(day)" class="absolute bottom-0 w-1.5 h-1.5 rounded-full bg-libelo-500 focus:text-white"></span>
                     </div>
                 </div>
-
-                <div v-if="calendarEvents.length" class="mt-6 bg-gray-800 rounded-md">
-                    <ul class="list-disc list-inside text-sm text-gray-700">
-                        <li v-for="event in getEventsforSelectedDays()" :key="event.id"
-                            class="border border-white text-white px-3 pt-4 pb-4">
-                            {{ event.summary }} - {{ event.start.dateTime || event.start.date }}
-                        </li>
-                    </ul>
-                </div>
+                <BaseTitle v-if="getEventsforSelectedDays().length" :title="`Eventos para el día ${selectedDateLabel}`">
+                    <div v-if="calendarEvents.length" v-for="event in getEventsforSelectedDays()" :key="event.id" @click="openEventModal(event)" class="p-2 flex justify-between items-center text-white bg-libelo-500 hover:bg-libelo-600 rounded-xl overflow-hidden">
+                        <div class="flex flex-col overflow-hidden pl-1">
+                            <span class="text-xs whitespace-nowrap">{{ formatDateTime(event.start) }}</span>
+                            <span class="font-semibold line-clamp-1 break-all">{{ event.summary }}</span>
+                        </div>
+                        <button class="size-11 flex items-center justify-center hover:bg-libelo-600 rounded-lg shrink-0" @click.stop="confirmDelete(event)"><Trash2 :size="20" /></button>
+                    </div>
+                </BaseTitle>
             </BaseTitle>
         </div>
+
+        <EditModal :show="showEventModal" :event="selectedEvent" :isEditing="isEditing" :date="formattedEditDate" @close="showEventModal = false" @update="handleUpdate" @delete="handleDelete" />
+        <DeleteEventModal :show="deleteEventModal" :event="eventToDelete" @close="deleteEventModal = false" @confirm="deleteConfirmedEvent" />
     </BaseBody>
 </template>
