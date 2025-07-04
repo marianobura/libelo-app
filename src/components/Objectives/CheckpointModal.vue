97fcaa5dba961b6b00373688e263dee2c37b59f0
marianobura <marianoobura@gmail.com>
2025-07-04 15:45:20 -0300

    Add DeleteModal and CheckpointModal components for objective management

---
+++ src/components/Objectives/CheckpointModal.vue
@@ -0,0 +1,35 @@
+<script setup>
+import BaseModal from "@/components/BaseModal.vue";
+import BaseButton from "@/components/BaseButton.vue";
+import { Check, X } from "lucide-vue-next";
+import { defineProps, defineEmits } from "vue";
+
+const props = defineProps({
+    showModal: Boolean,
+    objective: Object,
+    index: Number,
+});
+
+const emit = defineEmits(["close"]);
+
+const closeModal = () => emit("close");
+</script>
+
+<template>
+    <BaseModal :show="props.showModal" class="items-center justify-center" @close="closeModal">
+        <div class="bg-white p-4 rounded-xl w-full mx-2">
+            <h2 class="text-lg font-semibold break-all line-clamp-1">Objetivo {{ props.index + 1 }}: {{ props.objective?.text }}</h2>
+            <div class="flex items-center gap-2 mt-3 mb-3">
+                <span v-if="props.objective?.completed" class="text-libelo-500 font-bold flex items-center">
+                    <Check class="mr-1" /> Completado
+                </span>
+                <span v-else class="text-red-500 font-bold flex items-center">
+                    <X class="mr-1" /> No completado
+                </span>
+            </div>
+            <div class="flex justify-end mt-3">
+                <BaseButton @click="closeModal" secondary>Cerrar</BaseButton>
+            </div>
+        </div>
+    </BaseModal>
+</template>
---
+++ src/components/Objectives/DeleteModal.vue
@@ -0,0 +1,29 @@
+<script setup>
+import BaseModal from "@/components/BaseModal.vue";
+import BaseButton from "@/components/BaseButton.vue";
+import { defineProps, defineEmits } from "vue";
+
+const props = defineProps({
+    showModal: Boolean,
+});
+
+const emit = defineEmits(["close", "confirm"]);
+
+const closeModal = () => emit("close");
+const confirm = () => emit("confirm");
+</script>
+
+<template>
+    <BaseModal :show="props.showModal" class="items-center justify-center" @close="closeModal">
+        <div class="bg-white p-4 rounded-xl mx-2">
+            <div class="flex flex-col gap-1 w-full p-2">
+                <p class="font-semibold">Eliminar objetivos</p>
+                <p class="text-sm text-neutral-700">¿Está seguro de eliminar todos los objetivos actuales?</p>
+            </div>
+            <div class="flex justify-end mt-3 gap-4">
+                <BaseButton @click="closeModal" secondary>Cancelar</BaseButton>
+                <BaseButton @click="confirm" danger>Eliminar</BaseButton>
+            </div>
+        </div>
+    </BaseModal>
+</template>
