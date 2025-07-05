043859d19c97d16884da469bf8fd0f7ad6535304
marianobura <marianoobura@gmail.com>
2025-07-05 00:55:06 -0300

    Refactor DeleteEventModal: Update props, remove unused function, and enhance UI with alert icon

--- src/components/Calendar/DeleteEventModal.vue
+++ src/components/Calendar/DeleteEventModal.vue
@@ -2,10 +2,10 @@
 import { defineProps, defineEmits } from 'vue';
 import BaseModal from '@/components/BaseModal.vue';
 import BaseButton from '@/components/BaseButton.vue';
-import BaseTitle from '../BaseTitle.vue';
+import { TriangleAlert } from 'lucide-vue-next';
 
 const props = defineProps({
-    show: Boolean,
+    showModal: Boolean,
     event: Object
 });
 
@@ -16,47 +16,25 @@ const closeModal = () => {
 };
 
 const handleConfirm = () => emit('confirm');
-
-function formatFullDateTime(dateStr) {
-    if (!dateStr) return '';
-    const d = new Date(dateStr);
-    if (isNaN(d)) return '';
-
-    const date = d.toLocaleDateString('es-AR');
-    const time = d.toLocaleTimeString('es-AR', {
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false
-    });
-
-    return `${date} a las ${time}`;
-}
 </script>
 
 <template>
-    <BaseModal :show="props.show" class="items-center justify-center" @close="closeModal">
-        <div class="bg-white p-4 rounded-xl max-w-md w-full mx-2 max-h-[80vh] overflow-y-auto">
-            <BaseTitle title="¿Estás seguro de eliminar este evento?" description="Si lo eliminas, se borrará definitivamente.">
-                <div class="flex flex-col">
-                    <p class="text-semibold">
-                        <span class="font-semibold">Evento:</span>
-                        {{ props.event?.summary || 'Sin título' }}
-                    </p>
-                    <div class="flex flex-col">
-                        <p class="text-sm">
-                            <span class="font-semibold">Desde:</span>
-                            {{ formatFullDateTime(props.event?.start.dateTime) }}
-                        </p>
-                        <p class="text-sm">
-                            <span class="font-semibold">Hasta:</span>
-                            {{ formatFullDateTime(props.event?.end.dateTime) }}
-                        </p>
-                    </div>
+    <BaseModal :show="props.showModal" class="justify-center items-center" @close="closeModal">
+        <div class="w-full m-2 rounded-xl overflow-hidden">
+            <div class="flex gap-4 bg-white p-4">
+                <div class="size-12 flex items-center justify-center bg-red-100 rounded-full text-red-500 flex-shrink-0">
+                    <TriangleAlert size="24" />
                 </div>
-            </BaseTitle>
-            <div class="flex justify-end mt-4 gap-2">
-                <BaseButton @click="closeModal" secondary>Cancelar</BaseButton>
-                <BaseButton @click="handleConfirm" danger>Eliminar</BaseButton>
+                <div class="flex flex-col gap-1 w-full">
+                    <p class="font-semibold">Eliminar evento</p>
+                    <p class="text-sm text-neutral-700">Estás por eliminar <span class="underline">{{ props.event?.summary || 'Evento sin título' }}</span>. Esta acción no se puede deshacer y el evento ya no estará en tu calendario.</p>
+                </div>
+            </div>
+            <div class="bg-neutral-100 p-4">
+                <div class="flex justify-end gap-2">
+                    <BaseButton @click="closeModal">Cancelar</BaseButton>
+                    <BaseButton danger @click="handleConfirm">Eliminar</BaseButton>
+                </div>
             </div>
         </div>
     </BaseModal>
