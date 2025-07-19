2e035f947e70b4a8d253ecad5522174ff0e17d8c
marianobura <marianoobura@gmail.com>
2025-07-19 00:28:50 -0300

    Add subject management actions and state to subjectStore

--- src/stores/subjectStore.js
+++ src/stores/subjectStore.js
@@ -1,15 +1,21 @@
 import { defineStore } from "pinia";
 import axios from "axios";
+import { useUserStore } from "./userStore";
 
 export const useSubjectStore = defineStore("subjectStore", {
     state: () => ({
         subject: null,
+        subjects: [],
+        subjectsLoaded: false,
         loading: false,
     }),
 
     getters: {
         getSubjectData: (state) => {
             return state.subject;
+        },
+        getSubjects: (state) => {
+            return state.subjects;
         },
     },
 
@@ -30,5 +36,38 @@ export const useSubjectStore = defineStore("subjectStore", {
                 this.loading = false;
             }
         },
+
+        async fetchSubjects() {
+            if (this.subjectsLoaded) return;
+
+            const userStore = useUserStore();
+
+            this.loading = true;
+            try {
+                const apiUrl = new URL(`/api/subjects?studentId=${userStore.user._id}`, process.env.VUE_APP_API_URL);
+                const response = await axios.get(apiUrl.toString());
+                this.subjects = response.data.data;
+                this.subjectsLoaded = true;
+            } catch (error) {
+                console.error("Error al obtener las materias:", error);
+            } finally {
+                this.loading = false;
+            }
+        },
+
+        updateSubject(updatedSubject) {
+            const index = this.subjects.findIndex(s => s._id === updatedSubject._id);
+            if (index !== -1) {
+                this.subjects[index] = { ...this.subjects[index], ...updatedSubject };
+            }
+        },
+
+        addSubject(newSubject) {
+            this.subjects.push(newSubject);
+        },
+
+        removeSubject(subjectId) {
+            this.subjects = this.subjects.filter(subject => subject._id !== subjectId);
+        }
     }
 });
