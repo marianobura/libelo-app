c5fafe2f1438bf503f21e0d056a295253076d6fb
marianobura <marianoobura@gmail.com>
2025-07-28 20:37:11 -0300

    Update styles in RedeemModal and PromotionsListView components for improved UI consistency

--- src/components/Promotions/RedeemModal.vue
+++ src/components/Promotions/RedeemModal.vue
@@ -25,7 +25,7 @@ const redeem = () => {
     <BaseModal :show="props.showModal" class="justify-center items-center" @close="closeModal">
         <div class="bg-white rounded-xl w-full h-fit mx-2 overflow-hidden p-4">
             <BaseTitle title="Canjear promoción" :description="`¿Estás seguro de que deseas canjear esta promoción por ${promotion.points} puntos?`" />
-            <p v-if="props.error" class="text-red-500 text-sm">{{ props.error }}</p>
+            <p v-if="props.error" class="text-red-500 text-sm font-semibold mt-2">{{ props.error }}</p>
             <div class="flex justify-end mt-4 gap-2">
                 <BaseButton @click="closeModal" danger>Cancelar</BaseButton>
                 <BaseButton @click="redeem" primary>Canjear</BaseButton>
